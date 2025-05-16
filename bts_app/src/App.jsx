4caef82bbc0affe76778b676ba2903e2561f9995
chrstnmmnl <christianemmanuelcollo23@gmail.com>
2025-05-16 13:13:48 +0800

    setting up vite and react and tailwind

--- bts_app/src/App.jsx
+++ bts_app/src/App.jsx
@@ -3,7 +3,7 @@ import "./index.css";
 function App() {
 	return (
 		<>
-			<h1 className="text-3xl font-bold uderline">Welcome to React and Vite</h1>
+			<h1 className="text-5xl font-bold uderline">Welcome to React and Vite</h1>
 		</>
 	);
 }
